e55854cec6044b2e285148a5f802b47f923eeeea
unknown <raulknopa2@gmail.com>
2022-01-25 10:12:13 -0300

    Project created

---
+++ Backend/src/main/java/com/devsuperior/DSdeliver/DSdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSdeliverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/devsuperior/DSdeliver/DSdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.DSdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
